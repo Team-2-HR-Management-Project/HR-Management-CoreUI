58e333609fa09efbbe18aec338bc702e72ef9dfc
İlkiz İrem Çabuk <ilkizyoruk@gmail.com>
2022-12-20 01:06:46 +0300

    Image Update

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { CFooter } from '@coreui/react'
+import { CFooter, CLink } from '@coreui/react'
 
 const AppFooter = () => {
-  return <CFooter></CFooter>
+  return (
+    <CFooter>
+      <div>
+        <CLink href="https://coreui.io">CoreUI</CLink>
+        <span>&copy; 2021 creativeLabs.</span>
+      </div>
+      <div>
+        <span>Powered by</span>
+        <CLink href="https://coreui.io">CoreUI</CLink>
+      </div>
+    </CFooter>
+  )
 }
 
 export default React.memo(AppFooter)
