c15c33035bd072aca99f76f20c2599cd293a395c
Sezer Lucian Turkmen <74083623+sezerlucianturkmen@users.noreply.github.com>
2022-12-28 14:06:55 +0300

    update

--- src/config/RestApiUrls.js
+++ src/config/RestApiUrls.js
@@ -4,12 +4,12 @@ const companyPort = '8093'
 const version = '/api/v1'
 
 const RestApis = {
-  // authService: 'http://10.80.2.247:' + authPort + version,
-  // userService: 'http://10.80.5.244:' + userPort + version,
-  // companyService: 'http://10.80.13.28:' + companyPort + version,
-  authService: 'http://localhost:' + authPort + version,
-  userService: 'http://localhost:' + userPort + version,
-  companyService: 'http://localhost:' + companyPort + version,
+  authService: 'http://10.80.4.19:' + authPort + version,
+  userService: 'http://10.80.6.168:' + userPort + version,
+  companyService: 'http://10.80.7.101:' + companyPort + version,
+  //authService: 'http://localhost:' + authPort + version,
+  //userService: 'http://localhost:' + userPort + version,
+  //companyService: 'http://localhost:' + companyPort + version,
 }
 
 export default RestApis
